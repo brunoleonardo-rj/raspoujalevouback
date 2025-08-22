9c2e48c43d700763d6bae9976d62c34083a7d1af
brunoleonardo-rj <91506475+brunoleonardo-rj@users.noreply.github.com>
2025-08-22 13:40:44 -0300

    Update migration.sql

--- prisma/migrations/20250721061550_add_collumn_is_featured/migration.sql
+++ prisma/migrations/20250721061550_add_collumn_is_featured/migration.sql
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 -- AlterTable
 ALTER TABLE "scratch_cards" ADD COLUMN     "is_featured" BOOLEAN NOT NULL DEFAULT false;
-=======
--- AlterTable
-ALTER TABLE "scratch_cards" ADD COLUMN     "is_featured" BOOLEAN NOT NULL DEFAULT false;
->>>>>>> 0afbe7bb440a2cd7fa92381b5002449f20f09162
