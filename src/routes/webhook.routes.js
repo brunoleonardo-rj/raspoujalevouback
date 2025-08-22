97c470c666cf2526be049832f61f8438c40e4c0a
brunoleonardo-rj <91506475+brunoleonardo-rj@users.noreply.github.com>
2025-08-22 13:20:00 -0300

    Update webhook.routes.js

--- src/routes/webhook.routes.js
+++ src/routes/webhook.routes.js
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
 const express = require('express');
 const router = express.Router();
 const depositController = require('../controllers/deposit.controller');
 
 router.post('/webhook/:gateway', depositController.processWebhook);
 
-=======
-const express = require('express');
-const router = express.Router();
-const depositController = require('../controllers/deposit.controller');
-
-router.post('/webhook/:gateway', depositController.processWebhook);
-
->>>>>>> 0afbe7bb440a2cd7fa92381b5002449f20f09162
+=
 module.exports = router;
