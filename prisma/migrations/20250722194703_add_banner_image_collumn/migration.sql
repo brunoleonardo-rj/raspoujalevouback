f6ef2829fbfd2601120a233277ff48165ac3264b
brunoleonardo-rj <91506475+brunoleonardo-rj@users.noreply.github.com>
2025-08-22 13:41:07 -0300

    Update migration.sql

--- prisma/migrations/20250722194703_add_banner_image_collumn/migration.sql
+++ prisma/migrations/20250722194703_add_banner_image_collumn/migration.sql
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 -- AlterTable
 ALTER TABLE "settings" ADD COLUMN     "deposit_banner" TEXT;
-=======
--- AlterTable
-ALTER TABLE "settings" ADD COLUMN     "deposit_banner" TEXT;
->>>>>>> 0afbe7bb440a2cd7fa92381b5002449f20f09162
